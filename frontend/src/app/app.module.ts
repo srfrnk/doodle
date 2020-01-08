72a5de1cde721eb659bea6d10781966b52176c26
srfrnk <srfrnk@gmail.com>
2020-01-08 09:19:58 +0200

    app menu opacity control

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AgmCoreModule } from '@agm/core';
 import { MapComponent } from './map/map.component';
@@ -36,6 +37,7 @@ import { MapComponent } from './map/map.component';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    MatCheckboxModule,
     AgmCoreModule.forRoot({
       // please get your own API key here:
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from '../api.service';
+import { MatSliderChange } from '@angular/material/slider';
 
 @Component({
   selector: 'app-map',
@@ -16,18 +17,28 @@ export class MapComponent implements OnInit {
   lat = 51.51;
   lng = -0.12;
 
+  @Input('crimesOpacity')
+  set crimesOpacity(opacity: number) {
+    this.heatmap.set('opacity', opacity / 100.0);
+  }
+  get crimesOpacity(): number {
+    return !this.heatmap ? 0 : this.heatmap.get('opacity') * 100;
+  }
+
   async onMapLoad(mapInstance: google.maps.Map) {
     this.map = mapInstance;
+    if (!!this.heatmap) {
+      this.heatmap.setMap(this.map);
+    }
+  }
 
+  async ngOnInit() {
     const crimes = await this.api.crimes().toPromise();
     this.heatmap = new google.maps.visualization.HeatmapLayer({
       map: this.map,
       radius: 50,
-      opacity: 0.4,
+      opacity: 0.0,
       data: crimes.map(crime => ({ location: new google.maps.LatLng(crime.lat, crime.lon), weight: 1 })),
     });
-  }
-
-  ngOnInit() {
   }
 }
