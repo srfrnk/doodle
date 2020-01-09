df33a6fad6f7d58747109d5085e5f72bb44f099d
srfrnk <srfrnk@gmail.com>
2020-01-09 11:33:01 +0200

    refactoring + crimes gardient

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,6 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from '../api.service';
-import { MatSliderChange } from '@angular/material/slider';
+
+class Layer {
+  constructor(private setup: () => Promise<google.maps.visualization.HeatmapLayer>) {
+  }
+
+  private layer: google.maps.visualization.HeatmapLayer;
+
+  async load() {
+    this.layer = await this.setup();
+  }
+
+  get opacity(): number {
+    return !this.layer ? 0 : this.layer.get('opacity') * 100;
+  }
+  set opacity(value: number) {
+    if (!!this.layer) {
+      this.layer.set('opacity', value / 100.0);
+    }
+  }
+}
 
 @Component({
   selector: 'app-map',
@@ -8,37 +27,39 @@ import { MatSliderChange } from '@angular/material/slider';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService) {
+  }
 
   private map: google.maps.Map = null;
-  private heatmap: google.maps.visualization.HeatmapLayer = null;
+
+  public layers = {
+    crimes: new Layer(async () => {
+      const crimes = await this.api.crimes().toPromise();
+      const layer = new google.maps.visualization.HeatmapLayer({
+        map: this.map,
+        radius: 50,
+        opacity: 0.0,
+        gradient: [
+          'rgba(255, 0, 0, 0)',
+          'rgba(255, 0, 0, 1)',
+        ],
+        data: crimes.map(crime => ({ location: new google.maps.LatLng(crime.lat, crime.lon), weight: 1 }))
+      });
+      return layer;
+    })
+  };
 
   zoom = 14;
   lat = 51.51;
   lng = -0.12;
 
-  @Input('crimesOpacity')
-  set crimesOpacity(opacity: number) {
-    this.heatmap.set('opacity', opacity / 100.0);
-  }
-  get crimesOpacity(): number {
-    return !this.heatmap ? 0 : this.heatmap.get('opacity') * 100;
-  }
-
-  async onMapLoad(mapInstance: google.maps.Map) {
+  onMapLoad(mapInstance: google.maps.Map) {
     this.map = mapInstance;
-    if (!!this.heatmap) {
-      this.heatmap.setMap(this.map);
+    for (const layer of Object.values(this.layers)) {
+      layer.load();
     }
   }
 
-  async ngOnInit() {
-    const crimes = await this.api.crimes().toPromise();
-    this.heatmap = new google.maps.visualization.HeatmapLayer({
-      map: this.map,
-      radius: 50,
-      opacity: 0.0,
-      data: crimes.map(crime => ({ location: new google.maps.LatLng(crime.lat, crime.lon), weight: 1 })),
-    });
+  ngOnInit() {
   }
 }
